145d743218cae2a648bf1e0e6694b5454e94d0b9
Roshan Paudel <rozanpoudel@gmail.com>
2024-10-24 15:54:05 +0545

    👌 IMPROVE: fixes of search and implemented clear search

--- src/components/dashboard/Search.tsx
+++ src/components/dashboard/Search.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useState, useEffect } from "react";
 import { Input } from "../ui/input";
 import { useSearchParams, useRouter } from "next/navigation";
+import { CircleX } from "lucide-react";
 
 const DEBOUNCE_DELAY = 300; // Adjust delay as needed
 
@@ -14,6 +15,7 @@ const Search = () => {
   // Debounced effect
   useEffect(() => {
     const handler = setTimeout(() => {
+      if (!searchTerm) return;
       const newSearchParams = new URLSearchParams(searchParams);
       newSearchParams.set("searchQuery", searchTerm);
       router.push(`?${newSearchParams.toString()}`);
@@ -29,14 +31,30 @@ const Search = () => {
     setSearchTerm(e.target.value); // Set the search term but delay the actual search
   }
 
+  function handleClearSearch() {
+    const newSearchParams = new URLSearchParams(searchParams);
+    setSearchTerm("");
+    newSearchParams.delete("searchQuery", searchTerm);
+    router.push(`?${newSearchParams.toString()}`);
+  }
+
   return (
-    <Input
-      // defaultValue={query || ""}
-      value={searchTerm}
-      onChange={handleSearch}
-      placeholder="Search tasks..."
-      className="md:flex-1"
-    />
+    <div className="relative w-full">
+      <Input
+        value={searchTerm}
+        onChange={handleSearch}
+        placeholder="Search tasks..."
+        className="md:flex-1"
+      />
+      {!!searchTerm && (
+        <CircleX
+          role="button"
+          onClick={handleClearSearch}
+          className="text-destructive/80 absolute right-1 top-1/2 -translate-y-1/2"
+          size={20}
+        />
+      )}
+    </div>
   );
 };
 
